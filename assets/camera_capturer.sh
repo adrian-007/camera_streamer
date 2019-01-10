1aa9535577345a292d9c148d9a8063f35736a7f8
Adrian Guzowski <adrian-007@o2.pl>
2019-01-10 20:14:03 +0100

    * Reformatted arguments in capturer script * Changed H264 profile and level so that it compresses better * Changed segment duration to 1 - it's capturing until next I-frame arrives, so   there's no point in setting exact time anyway and with low value we'll be sure   that slicing is done at I-frame, exactly * Added some extra options to raspivid instance (flushing buffers, inline headers).

--- assets/camera_capturer.sh
+++ assets/camera_capturer.sh
@@ -14,15 +14,44 @@
 [ "$CAMERA_FPS" -eq "$CAMERA_FPS" ] 2>/dev/null && [ $CAMERA_FPS -gt 0 ] && [ $CAMERA_FPS -le 30 ] || exit 1
 [ "$CAMERA_SEGMENT_DURATION" -eq "$CAMERA_SEGMENT_DURATION" ] 2>/dev/null && [ $CAMERA_SEGMENT_DURATION -gt 0 ] || exit 1
 
-trap 'kill -TERM $PID' INT TERM
+trap 'trap "" INT TERM; kill -TERM $PID; wait $PID;' INT TERM
 
 raspivid \
-    -fps $CAMERA_FPS -w $CAMERA_WIDTH -h $CAMERA_HEIGHT -n -t 0 -pf baseline -lev 4 \
-    -qp 20 -b 0 -g $(($CAMERA_FPS*$CAMERA_SEGMENT_DURATION)) -cfx 128:128 -a 1032 -a "%Y-%m-%d %X" -ae 16,0xff,0x808033,2,20,0 -o - | \
-    ffmpeg -loglevel fatal \
-        -r $CAMERA_FPS -f h264 -probesize 8096 -analyzeduration 100000 -fflags +genpts -i - -codec:v copy -f hls \
-        -hls_list_size 0 -hls_time $(($CAMERA_SEGMENT_DURATION-1)) -use_localtime 1 -use_localtime_mkdir 1 \
-        -hls_flags temp_file+independent_segments -hls_segment_filename "$CAMERA_BASE_DIR/%Y-%m-%d/video_%H%M%S.ts" \
+    -t 0 \
+    -n \
+    -fps $CAMERA_FPS \
+    -w $CAMERA_WIDTH \
+    -h $CAMERA_HEIGHT \
+    -ih \
+    -fl \
+    -stm \
+    -pf high \
+    -lev 4.1 \
+    -qp 20 \
+    -b 0 \
+    -g $(($CAMERA_FPS*$CAMERA_SEGMENT_DURATION)) \
+    -cfx 128:128 \
+    -a 1032 \
+    -a "%Y-%m-%d %X" \
+    -ae 16,0xff,0x808033,2,20,0 \
+    -o - | \
+    ffmpeg \
+        -loglevel error \
+        -hide_banner \
+        -f h264 \
+        -probesize 2048 \
+        -analyzeduration 1 \
+        -r $CAMERA_FPS \
+        -re \
+        -i - \
+        -c:v copy \
+        -f hls \
+        -hls_list_size 0 \
+        -hls_time 1 \
+        -strftime \
+        -strftime_mkdir 1 \
+        -hls_flags temp_file+independent_segments \
+        -hls_segment_filename "$CAMERA_BASE_DIR/%Y-%m-%d/video_%H%M%S.ts" \
         /dev/null &
 
 # This trickery is needed for properly killing child processes (ffmpeg & raspivid) so that they
@@ -31,8 +60,6 @@ raspivid \
 # Idea from http://veithen.io/2014/11/16/sigterm-propagation.html
 
 PID=$!
-wait $PID
-trap - INT TERM
 wait $PID
 
 exit 0;
